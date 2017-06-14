608475c2ffbc0ad19b7bd96c2271f0b7b08d0d15
Allan.W.C <allan_195@hotmail.com>
2017-06-15 09:04:02 +1000

    adding negative function
    not working yet

--- javascript.js
+++ javascript.js
@@ -1,275 +1,278 @@
 
 //no. variables
-var zero    = "0";
-var one     = "1";
-var two     = "2";
-var three   = "3";
-var four    = "4";
-var five    = "5";
-var six     = "6";
-var seven   = "7";
-var eight   = "8";
-var nine    = "9";
+var zero = '0'
+var one = '1'
+var two = '2'
+var three = '3'
+var four = '4'
+var five = '5'
+var six = '6'
+var seven = '7'
+var eight = '8'
+var nine = '9'
 
-var string = "";
-var digits = "";
-var arr = [];
-var operator = [];
-var order = [];
-var numbers = [];
-var val = 0;
-var background = 0;
+var string = ''
+var digits = ''
+var arr = []
+var operator = []
+var order = []
+var numbers = []
+var val = 0
+var background = 0
 
 //display function
-function display(){
-  document.getElementById('log').innerHTML = string;
+function display () {
+  document.getElementById('log').innerHTML = string
 }
 
 //assigning numbers
-function zeroFunction(){
-  if (string.length <12){
-  string += zero;
-  display();
-  digits += zero;
+function zeroFunction () {
+  if (string.length < 12) {
+      string += zero
+      display()
+      digits += zero
  }
 }
-function oneFunction(){
-  if (string.length <12){
-  string += one;
-  display();
-  digits += one;
+function oneFunction () {
+  if (string.length < 12) {
+      string += one
+      display()
+      digits += one
  }
 }
-function twoFunction(){
-  if (string.length <12){
-  string += two;
-  display();
-  digits += two;
+function twoFunction () {
+  if (string.length < 12) {
+      string += two
+      display()
+      digits += two
  }
 }
-function threeFunction(){
-  if (string.length <12){
-  string += three;
-  display();
-  digits += three;
+function threeFunction () {
+  if (string.length < 12) {
+      string += three
+      display()
+      digits += three
  }
 }
-function fourFunction(){
-  if (string.length <12){
-  string += four;
-  display();
-  digits += four;
+function fourFunction () {
+  if (string.length < 12) {
+      string += four
+      display()
+      digits += four
  }
 }
-function fiveFunction(){
-  if (string.length <12){
-  string += five;
-  display();
-  digits += five;
+function fiveFunction () {
+  if (string.length < 12) {
+      string += five
+      display()
+      digits += five
  }
 }
-function sixFunction(){
-  if (string.length <12){
-  string += six;
-  display();
-  digits += six;
+function sixFunction () {
+  if (string.length < 12) {
+      string += six
+      display()
+      digits += six
  }
 }
-function sevenFunction(){
-  if (string.length <12){
-  string += seven;
-  display();
-  digits += seven;
+function sevenFunction () {
+  if (string.length < 12) {
+      string += seven
+      display()
+      digits += seven
  }
 }
-function eightFunction(){
-  if (string.length <12){
-  string += eight;
-  display();
-  digits += eight;
+function eightFunction () {
+  if (string.length < 12) {
+      string += eight
+      display()
+      digits += eight
  }
 }
-function nineFunction(){
-  if (string.length <12){
-  string += nine;
-  display();
-  digits += nine;
+function nineFunction () {
+  if (string.length < 12) {
+      string += nine
+      display()
+      digits += nine
  }
 }
 
 //assigning functions
-function clearFunction(){
-  string = "";
-  val = 0;
-  digits = "";
-  arr = [];
-  operator = [];
-  numbers = [];
-  order = [];
-  display();
+function clearFunction () {
+  string = ''
+  val = 0
+  digits = ''
+  arr = []
+  operator = []
+  numbers = []
+  order = []
+  display()
 }
-function clearHistoryFunction(){
-  document.getElementById('history').innerHTML = "";
+function clearHistoryFunction () {
+  document.getElementById('history').innerHTML = ''
 }
-function backgroundFunction(){
-switch (background) {
-  case 0:
-    document.body.style.backgroundImage = "url('http://i.imgur.com/weTCs21.png')"
-    background += 1;
-    break;
-  case 1:
-    document.body.style.backgroundImage = "url('http://i.imgur.com/UrppsLd.png')"
-    background += 1;
-    break;
-  case 2:
-    document.body.style.backgroundImage = "url('http://i.imgur.com/VsXkIeB.jpg')"
-    background += 1;
-    break;
-  case 3:
-    document.body.style.backgroundImage = "url('http://i.imgur.com/eJ0SiUb.png')"
-    background += 1;
-    break;
-  case 4:
-    document.body.style.backgroundImage = "url('http://i.imgur.com/ZuppCXb.png')"
-    background = 0;
-    break;
-  default:
- }
-}
-function delFunction(){
-  if (/\d$/.test(string)){
-    var splitDel2 = digits.split("");
-  splitDel2.pop();
-  digits = splitDel2.join("");
+
+function delFunction () {
+  if (/\d$/.test(string)) {
+    var splitDel2 = digits.split('')
+      splitDel2.pop()
+      digits = splitDel2.join('')
 }else{
-  console.log(operator);
-  operator.pop();
+  operator.pop()
 }
-var splitDel = string.split("");
-splitDel.pop();
-string = splitDel.join("");
-  display();
-  console.log(operator);
+var splitDel = string.split('')
+splitDel.pop()
+string = splitDel.join('')
+  display()
 }
-function decimalFunction(){
-  if (string.length <12){
-  string += ".";
-  display();
+function decimalFunction () {
+  if (string.length < 12) {
+      string += "."
+      display()
  }
 }
-function plusFunction(){
- if (string.length <12){
-   if (string == ""){
-     arr.push(val);
+function plusFunction () {
+ if (string.length < 12) {
+   if (string === '') {
+     arr.push(val)
      string += val
    }
-  string += "+";
-  display();
-  if (digits.length > 0){
-  arr.push(Number(digits));
-  digits = "";
+  string += '+'
+  display()
+  if (digits.length > 0) {
+      arr.push(Number(digits))
+      digits = ''
   }
-  operator.push("+");
+  operator.push('+')
  }
 }
-function minusFunction(){
- if (string.length <12){
-   if (string == ""){
-     arr.push(val);
+function minusFunction () {
+ if (string.length < 12) {
+   if (string === '') {
+     arr.push(val)
      string += val
    }
-  string += "-";
-  display();
-  if (digits.length > 0){
-  arr.push(Number(digits));
-  digits = "";
+  string += '-'
+  display()
+  if (digits.length > 0) {
+      arr.push(Number(digits))
+      digits = ''
   }
-  operator.push("-");
+  operator.push('-')
  }
 }
-function multiplyFunction(){
- if (string.length <12){
-   if (string == ""){
-     arr.push(val);
+function multiplyFunction () {
+ if (string.length < 12) {
+   if (string === '') {
+     arr.push(val)
      string += val
    }
-  string += "x";
-  display();
-  if (digits.length > 0){
-  arr.push(Number(digits));
-  digits = "";
+  string += 'x'
+  display()
+  if (digits.length > 0) {
+      arr.push(Number(digits))
+      digits = ''
   }
-  operator.push("x");
+  operator.push('x')
  }
 }
-function divideFunction(){
- if (string.length <12){
-   if (string == ""){
-     arr.push(val);
+function divideFunction () {
+ if (string.length < 12) {
+   if (string === '') {
+     arr.push(val)
      string += val
    }
-  string += "/";
+  string += '/'
   display();
-  if (digits.length > 0){
-  arr.push(Number(digits));
-  digits = "";
+  if (digits.length > 0) {
+      arr.push(Number(digits))
+      digits = ''
   }
-  operator.push("/");
+  operator.push('/')
  }
 }
 
-function equalsFunction(){
- arr.push(Number(digits));
+// changing to negative function
+function negative () {
+    var neg = 0;
+    let valStr = digits.charAt(0)
+    console.log(digits)
+    console.log(valStr)
+    let subIndex = string.lastIndexOf(valStr)
+    console.log(subIndex)
+    let subIndex2 = string.lastIndexOf(valStr) - 2
+    let subStr = string.substring(subIndex)
+    let subStr2 = string.substring(subIndex2)
+    console.log(subIndex)
+    console.log(subStr)
+if (neg === 0) {
+  string = string.slice(subIndex)
+    string = string.concat('(-' + subStr + ')')
+    display()
+    neg = 1
+} else {
+  string = string.replace(subStr2, subStr)
+  display()
+  neg = 0
+}
+  
+  val = val * -1
+
+}
+
+
+function equalsFunction () {
+ arr.push(Number(digits))
 
 //calculating multiply and divide
- for (var i=0; i<arr.length; i++){
-   var j = i + 1;
-   var h = i - 1;
-     if (operator[i] == "x"){
-      arr[j] = arr[i]*arr[j]
-      if (operator[j] != "x" && operator[j] != "/"){
-      numbers.push(arr[j]);
+ for (var i = 0; i < arr.length; i++) {
+   var j = i + 1
+   var h = i - 1
+     if (operator[i] === 'x') {
+      arr[j] = arr[i] * arr[j]
+      if (operator[j] !== 'x' && operator[j] !== '/') {
+      numbers.push(arr[j])
     }
-   }else if(operator[i] == "/"){
-     arr[j] = arr[i]/arr[j]
-     if (operator[j] != "x" && operator[j] != "/"){
-     numbers.push(arr[j]);
+  } else if (operator[i] === '/') {
+     arr[j] = arr[i] / arr[j]
+     if (operator[j] !== 'x' && operator[j] !== '/') {
+     numbers.push(arr[j])
     }
   }else {
-     order.push(operator[i]);
-     if(operator[h] != "x" && operator[h] != "/"){
-         numbers.push(arr[i]);
+     order.push(operator[i])
+     if (operator[h] !== 'x' && operator[h] !== '/') {
+         numbers.push(arr[i])
        }
    }
  }
 
 //calculating plus and minus
-val = numbers[0];
-for (var n=0; n<numbers.length; n++){
-  var o = n + 1;
+val = numbers[0]
+for (var n = 0; n < numbers.length; n++) {
+  var o = n + 1
   switch (order[n]) {
-    case "+": val += numbers[o];
-      break;
-    case "-": val -= numbers[o];
-      break;
+    case '+': val += numbers[o]
+      break
+    case '-': val -= numbers[o]
+      break
     default:
   }
 }
 
 //shortening val
-if(val.toString().length >12){
-val = val.toPrecision(12)
+if (val.toString().length > 12) {
+    val = val.toPrecision(12)
 }
 
  //display equals values
- document.getElementById('log').innerHTML = val;
- document.getElementById('history').innerHTML = string;
+ document.getElementById('log').innerHTML = val
+ document.getElementById('history').innerHTML = string
 
  //clear
- string = "";
- digits = "";
- arr = [];
- operator = [];
- numbers = [];
- order = [];
-}//equalsFunction
+ string = ''
+ digits = ''
+ arr = []
+ operator = []
+ numbers = []
+ order = []
+} //  equalsFunction
